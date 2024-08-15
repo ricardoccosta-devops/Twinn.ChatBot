1c2418fa53de56223dc3e10e29a75258f09b5b50
RICARDO CORDEIRO SOARES DA COSTA <71666942+ricardoccosta-devops@users.noreply.github.com>
2024-08-15 13:07:40 +0000

    change the name

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,9 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Twinn Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple chatbot that uses OpenAI models to generate responses. "
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
